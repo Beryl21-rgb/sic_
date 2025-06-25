6dd5845d0307f7eba36349c7f79e997ef95e56e6
Beryl21-rgb <beryltbinu120@gmail.com>
2025-06-25 14:47:48 +0530

    Update programs.py
    programs for leap year, perfect square and smallest number were performed

--- assignments/programs.py
+++ assignments/programs.py
@@ -1,16 +1,18 @@
-# Check if a year is leap year
+# 1. Check if a year is leap year
 year = int(input("Enter the Year that is to be checkked: "))
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("It is a Leap Year")
 
-#Check if a positive integer is a perfect square
+
+
+# 2. Check if a positive integer is a perfect square
 positive_integer=int(int(input("Enter the posotive integer: ")))
 if positive_integer > 0:
     for perfect_number in range(1,positive_integer):
         if perfect_number*perfect_number==positive_integer:
             print(positive_integer,"is a perfect square")
             
-#Find the smallest of 3 distinct numbers
+# 3. Find the smallest of 3 distinct numbers
 number_1=int(input("Enter the first number: "))
 number_2=int(input("Enter the second number: "))
 number_3=int(input("Enter the second number: "))
