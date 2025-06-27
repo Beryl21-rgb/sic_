e66cdafa235150e0ade444844bceb3b878f139cb
Beryl21-rgb <beryltbinu120@gmail.com>
2025-06-27 16:07:10 +0530

    Update p1.py
    Implementation of  Stack using list, insert and delete from rear of the list

--- week1/p1.py
+++ week1/p1.py
@@ -5,3 +5,31 @@ input_number = int(input)
 
 if input_number % 2 == 0:
     print(input_number,'is an Even number')
+
+# 1. Implement Stack using list, insert and delete from rear of the list
+
+l = []
+        
+def stack(choice):
+    match(choice):
+        case 1:
+            input_value = int(input("Enter value to be pushed: "))
+            l.append(input_value)
+        case 2:
+            l.pop()
+        case 3:
+            if len(l) == 0:
+                prnit("Stack is Emty")
+                return
+            else:
+                print(l)
+        case 4:
+            s.exit("End of program")
+
+        case _:
+            print('Invalid choice')   
+            
+while True:
+    print('1.Push  2.Pop  3.Display   4.exit')
+    choice = int(input("Stack Operation: "))
+    stack(choice)
