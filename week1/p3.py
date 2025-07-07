33851f0336088422d4fbb7a9d0ff5615c7f879ae
Beryl21-rgb <beryltbinu120@gmail.com>
2025-07-07 09:46:01 +0530

    updated week1 folder

--- week1/p3.py
+++ week1/p3.py
@@ -59,11 +59,28 @@ print(sum_of_series)
         
     
 #4. Print the Prime numbers in decreasing order between m and n (m < n)
-n = int(input("Enter the starting range: "))
-m = int(input("Enter the endig range "))
-prime_numbers = []
-for num in range(n, m+1):
-    for i in range(i+1,m+1):
-        if num % i == 0:
-            prime_numbers.append(num)
-print(prime_numbers)
+# n = int(input("Enter the starting range: "))
+# m = int(input("Enter the endig range "))
+# prime_numbers = []
+# for num in range(n, m+1):
+#     for i in range(i+1,m+1):
+#         if num % i == 0:
+#             prime_numbers.append(num)
+# print(prime_numbers)
+
+
+def isPrime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+print("Enter the range in such a way that start range should be less than end range to print it in decreasing order!!")
+start_range = int(input("Enter the start range to print the prime numbers: "))
+end_range = int(input("Enter the end range where it should stop printing: "))
+
+for i in range(end_range, start_range, -1):
+    if isPrime(i):
+        print(i, end = " ")
